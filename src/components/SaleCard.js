8223f5e733c4a5a420570ac02c056dbe922797da
Jacob Poling <jacob.poling@altamiracorp.com>
2023-07-12 08:37:29 -0400

    added Price Chip

--- src/components/SaleCard.js
+++ src/components/SaleCard.js
@@ -1,6 +1,8 @@
 import { Typography } from "@mui/material";
-import {Card, CardContent, CardMedia, CardActionArea} from "@mui/material";
+import {Card, CardContent, CardMedia, CardActionArea, Chip} from "@mui/material";
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import logo from "./W.PNG"
+
 
 function Salecard() {
   return (
@@ -17,6 +19,7 @@ function Salecard() {
           <Typography gutterBottom variant="h5" component="div">
             Leather Wallet
           </Typography>
+          <Chip avatar={<AttachMoneyIcon/>} label="20.00" size="small" variant="outlined"/>
           <Typography variant="body2" color="text.secondary">
             This is a wallet made of leather. You can tell its made of leather by the way its made of leather. The leather wallet knows its made of leather by the way it feels like leather.
           </Typography>
