f9307d6c6462152545b735a59f856fa3398a86a1
Jacob Poling <polingj999@gmail.com>
2023-07-13 06:32:10 -0400

    Finished positioning Category names and made button to see all Items

--- src/components/Categorydisplay.js
+++ src/components/Categorydisplay.js
@@ -1,17 +1,20 @@
-import { display, flexbox } from "@mui/system";
 import Salecard from "./SaleCard";
-import Grid from "@mui/material/Unstable_Grid2/Grid2";
-import { Typography } from "@mui/material";
+import { Typography ,Button} from "@mui/material";
+import EastIcon from '@mui/icons-material/East';
+
 function Categorydisplay() {
     return (
-        <div style={{display:'flex', flexDirection:'column'}}>
-            <Typography>Category Name</Typography>
-            <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                <Salecard />
-                <Salecard />
-                <Salecard />
-                <Salecard />
+        <div style={{ display: 'flex', flexDirection: 'column', padding: '20px',alignItems:'center', width:'100%'}}>
+            <div style={{display:'flex', flexDirection:'row',width:'70%', justifyContent:'space-between'}}>
+                <Typography variant="h3" >Category Name</Typography>
+                <Button variant="text" size="large" endIcon={<EastIcon/>} >See All</Button>
+            </div>
 
+            <div style={{ display: 'flex', justifyContent: 'space-evenly'}}>
+                <Salecard />
+                <Salecard />
+                <Salecard />
+                <Salecard />
             </div>
 
         </div>
