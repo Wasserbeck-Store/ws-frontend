c38a45c51a47fd26fb4662f7e45170650560d53b
Jacob Poling <polingj999@gmail.com>
2023-10-22 16:26:34 -0400

    switching systems

--- src/components/Categorydisplay.js
+++ src/components/Categorydisplay.js
@@ -5,9 +5,8 @@ import { Link } from "react-router-dom";
 
 
 function Categorydisplay({category}) {
-    const handleClick = () => {
-        console.log("This will Navigate to the product Page");
-    };
+
+
     return (
         <div style={{ display: 'flex', flexDirection: 'column', padding: '20px',alignItems:'center'}}>
             <div style={{display:'flex', flexDirection:'row',width:'70%', justifyContent:'space-between'}}>
@@ -16,7 +15,7 @@ function Categorydisplay({category}) {
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap'}}>
-                <Salecard category={category} id="000" component={Link} to="/product" onClick={handleClick}/>
+                <Salecard category={category} id="000" component={Link} to="/product" />
                 <Salecard category={category} id="001" component={Link} to="/product"/>
                 <Salecard  category={category} id="002" component={Link} to="/product"/>
                 <Salecard  category={category} id="003" component={Link} to="/product"/>
